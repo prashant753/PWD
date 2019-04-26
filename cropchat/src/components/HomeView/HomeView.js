b69f8cf47a246341a919049133f4d7385b6fdcfb
prashant <prashant@nineleaps.com>
2019-04-26 15:58:52 +0530

    feat: 🎸 Added images and nav bar

--- cropchat/src/components/HomeView/HomeView.js
+++ cropchat/src/components/HomeView/HomeView.js
@@ -1,2 +1,13 @@
+import data from '../../data'
 export default {
+  methods: {
+    displayDetails (id) {
+      this.$router.push({name: 'detail', params: { id: id }})
+    }
+  },
+  data () {
+    return {
+      'pictures': data.pictures
+    }
+  }
 }
